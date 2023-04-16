df7adfc3ac3a8dc722d8ed87855dd08b3f913ce6
hoshi005 <hoshi005@me.com>
2023-04-16 10:36:49 +0900

    Initial Commit

---
+++ dynamic-island/dynamic_islandApp.swift
@@ -0,0 +1,17 @@
+//
+//  dynamic_islandApp.swift
+//  dynamic-island
+//
+//  Created by Susumu Hoshikawa on 2023/04/16.
+//
+
+import SwiftUI
+
+@main
+struct dynamic_islandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
